a64eced69edf1a74c952da1fa766265d917cde8a
Raphael <raphaelarzoin173@gmail.com>
2025-01-27 20:45:07 +0100

    changement de preview

--- js/audio.js
+++ js/audio.js
@@ -2,3 +2,12 @@ function playMusic() {
     var audio = document.getElementById("myAudio");
     audio.play();
 }
+
+// Arrêter la musique lorsque l'utilisateur quitte la page
+window.addEventListener("beforeunload", function () {
+    var audio = document.getElementById("myAudio");
+    if (audio) {
+        audio.pause();
+        audio.currentTime = 0; // Réinitialiser à zéro
+    }
+});
